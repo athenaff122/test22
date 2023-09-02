ed1e95a3b06115822023a29ef8480dec9366fc2d
JY 范 <jyfan@JYdeMacBook-Pro.local>
2023-09-02 11:01:50 -0400

    Initial_com

---
+++ Desktop/2022_Winter/SI579/in-class-exercise-1-athenaff122-main/js/script.js
@@ -0,0 +1,28 @@
+// Remove me entirely!
+
+// uncomment below to fix tests
+document.querySelector('header h1').textContent = 'SI 579 Hello';
+
+const variable1 = "Hardcore UFOs";
+const variable2 = `Kicker of Elves`;
+const variable3 = "My Valuable Hunting Knife";
+const variable4 = "Watch me Jumpstart";
+
+const demonstrateConsoleLog = () => {
+  const courseNumber = "SI 579";
+  const courseDescription =
+    "Building Interactive Applications (in JavaScript and React)";
+};
+
+const demonstrateDebugger = () => {
+  const innerVariable1 = "Hulk Hogan";
+  const innerVariable2 = "Randy Macho Man Savage";
+};
+
+// DO NOT CHANGE ANYTHING BELOW THIS.
+document
+  .querySelector("#console")
+  .addEventListener("click", demonstrateConsoleLog);
+document
+  .querySelector("#debugger")
+  .addEventListener("click", demonstrateDebugger);
